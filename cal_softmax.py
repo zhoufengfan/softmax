0335295f159bd8237332b74015365d4cef5abfa4
zhoufengfan <zhfengfan@126.com>
2020-12-24 20:42:03 +0800

    change some string

--- cal_softmax.py
+++ cal_softmax.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     t1 = torch.tensor([1, 2, 3]).float()
     t2 = F.softmax(t1, dim=0)
     t3 = torch.exp(t1) / torch.sum(torch.exp(t1))
-    print("t2为：\t", t2)
-    print("t3为：\t", t3)
+    print("t2 is:\t", t2)
+    print("t3 is:\t", t3)
